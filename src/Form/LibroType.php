e7d5ffae3b1ff06c34cfcd9d583dbe74f8013a35
Your Name <you@example.com>
2022-01-14 13:51:18 -0500

    BootStrapult

--- src/Form/LibroType.php
+++ src/Form/LibroType.php
@@ -6,14 +6,20 @@ use App\Entity\Libro;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class LibroType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('titulo')
-            ->add('autor')
+        ->add('titulo', TextType::class,array(
+            'attr' => array(
+                'class' => 'form-control'
+            )
+        )) 
+
+        ->add('autor')
         ;
     }
 
